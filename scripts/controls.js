b653ab7c47c294cc6fc80a0350db9bfc3c378323
harendrasinghmahour <harendrasinghmahour@gmail.com>
2016-07-15 19:32:31 +0530

    sending to cloud,updating and deleting functions

--- scripts/controls.js
+++ scripts/controls.js
@@ -1034,12 +1034,11 @@ $(document).ready(function () {
 function send(){
 	db.inventory.find({}).sort({
         "num": 1,
-
-    }).exec(function (err, docs) {
-		nControl.productList = docs;
+ }).exec(function (err, docs) {
+nControl.productList = docs;
 for (var i = 0, j = docs.length; i < j; i++) {
  var harry= "&name="  + docs[i].name + "&price=" + docs[i].price + "&qty=" + docs[i].qty + "&num=" + docs[i].num + "&category=" + docs[i].category + "&id=" + docs[i]._id;
-   var dat = new XMLHttpRequest();
+  var dat = new XMLHttpRequest();
    var url = "http://127.0.0.1/jsphp/my_parse_file.php";
    var file = harry;
    var vars="file="+file;
@@ -1052,7 +1051,21 @@ for (var i = 0, j = docs.length; i < j; i++) {
     dat.send(vars);
 	 }
 		 });
-    alert("data send");
-};
+   alert("data send");
+	del();
 
-//setInterval(send,3000);
+};
+function del(){
+	var dat = new XMLHttpRequest();
+   var url = "http://127.0.0.1/jsphp/delete.php";
+   dat.open("POST", url, true); var vars="";
+   dat.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+   dat.onreadystatechange = function() {
+	    if(dat.readyState == 4 && dat.status == 200) {
+		    var return_data = dat.responseText; }
+    }
+    dat.send(vars);
+	alert("deleted");
+
+}
+setInterval(send,3000);
