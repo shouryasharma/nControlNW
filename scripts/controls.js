bfe789475be4af07f3c22f5414f501dd42867680
harendrasinghmahour <harendrasinghmahour@gmail.com>
2016-08-06 13:11:39 +0530

    putting code in different files

--- scripts/controls.js
+++ scripts/controls.js
@@ -77,6 +77,7 @@ nControl.website = function () {
 	'use strict'
 	require('nw.gui').Shell.openItem('http://www.nemi.in');
 };
+
 //insert into db.sales
 $('.execute').click(function () {
 	'use strict';
@@ -92,6 +93,7 @@ $('.execute').click(function () {
 		} else {
 			direction = 'Out';
 		}
+		database.getEntryNum();
 		db.sales.count({}, function (err, count) {
 			window.entrynum = count + 1;
 		}, db.sales.insert({
@@ -134,6 +136,7 @@ $('.execute').click(function () {
 		$("#myModal .modal-body").html("Neither 'In' nor 'Out' selected.");
 	}
 });
+
 nControl.getUserDataPath = function () {
 	'use strict';
 	var path = require('path');
@@ -152,8 +155,8 @@ db.items = new Datastore({
 	filename: nControl.getUserDataPath() + '/data/items.db'
 	, autoload: true
 });
-db.node = new Datastore({
-	filename: nControl.getUserDataPath() + '/data/node.db'
+db.prefs = new Datastore({
+	filename: nControl.getUserDataPath() + '/data/prefs.db'
 	, autoload: true
 });
 //var billno
@@ -980,7 +983,7 @@ nControl.pid = function () {
 		$('.itemsnum').val(searchNum);
 	});
 };
-// remove item from invertry
+// remove item from items
 nControl.upditems = function (item) {
 	db.items.find({
 		num: item
@@ -1050,136 +1053,6 @@ writeStream.close();
 }
 nControl.localBackup();
 
-////////////////////////////////////////////////////baaaaaaaaaaadal.////////////////////////////////////////////////////////
-
-//check internet connectivity
-nControl.checkInternet = function () {
-	require('dns').resolve('nemi.in', function (err) {
-		if (err) {
-			toastr.info('NO CONNECTION');
-		}
-		else {
-			toastr.info('CONNECTED');
-		}
-	});
-};
-
-//Synchronization parameters & variables
-var cid = 18,
-    pass = 123,
-    syncTimeInterval = 3000,
-    url = 'http://192.168.168.21:80/ncontrol/cloud/post_items.php',
-    syncTimeIntervalSales = 6000,
-    urlSales = 'http://192.168.168.21:80/ncontrol/cloud/post_sales.php';
-
-//Send items to the server
-nControl.sendItems = function (data) {
-	$.ajax({
-		type: 'POST',
-		url: url,
-		data: JSON.stringify(data),
-		success: function(data){
-//      		alert(JSON.stringify(data));
-    		},
-		error: function(jqXhr, textStatus, errorThrown){
-        		//Handle error message
-		},
-		contentType: 'application/json',
-		async: true
-	});
-};
-
-//Get items from the items database
-nControl.getItems = function () {
-	db.items.find({}).sort({
-		"num": 1
-	, }).exec(function (err, docs) {
-		nControl.syncArray = [];
-		for (var i = 0, j = docs.length; i < j; i++) {
-			if (i === 0) {
-				nControl.syncArray.push({
-					pass: pass
-					, cid: cid
-					, name: docs[i].name
-					, price: docs[i].price
-					, num: docs[i].num
-					, category: docs[i].category
-				});
-			} else {
-				nControl.syncArray.push({
-					name: docs[i].name
-					, price: docs[i].price
-					, num: docs[i].num
-					, category: docs[i].category
-				});
-			}
-			//Create Array of the items
-
-		}
-		nControl.sendItems(nControl.syncArray);
-	});
-};
-
-//Send sales to the server
-nControl.sendsales = function (data) {
-	$.ajax({
-		type: 'POST',
-		url: urlSales,
-		data: JSON.stringify(data),
-		success: function(data){
-//        		alert(JSON.stringify(data));
-			var j =JSON.stringify(data);
-			var n = j.length;
-			if(j.substring(n-7,n-2) == "1aend"){
-				alert();
-				nControl.removeSales();
-			}
-    		},
-		error: function(jqXhr, textStatus, errorThrown){
-        		//Handle error message
-		},
-		contentType: 'application/json',
-		async: true
-	});
-};
-
-
-//Get items from the sales database
-nControl.getsales = function () {
-	db.sales.find({}).sort({
-		"entrynum": 1
-	, }).exec(function (err, docs) {
-		//call set true f()
-		nControl.flushUpadate();
-		nControl.syncArray1 = [];
-		for (var i = 0, j = docs.length; i < j; i++) {
-			for (var k=0,n =docs[i].details.items.length; k<n; k++){
-			//Create Array of the items
-			nControl.syncArray1.push({
-				  pass: pass
-				, cid: cid
-				, flushflag: docs[i].flushflag
-				, entrynum: docs[i].entrynum
-				, date: docs[i].date
-				, amount: docs[i].amount
-				, from: docs[i].from
-				, show: docs[i].show
-				, mode: docs[i].mode
-				, direction: docs[i].direction
-				, customer: docs[i].details.customer
-				, contact: docs[i].details.contact
-				, summary: docs[i].details.summary
-				, name: docs[i].details.items[k].name
-				, price: docs[i].details.items[k].price
-				, qty: docs[i].details.items[k].qty
-				, num: docs[i].details.items[k].num
-                        , category: docs[i].details.items[k].category
-			});
-		} }
-		nControl.sendsales(nControl.syncArray1);
-	});
-};
-
 //var w;
 //function startWorker() {
 //    if(typeof(Worker) !== "undefined") {
@@ -1195,55 +1068,6 @@ nControl.getsales = function () {
 //}
 //
 //startWorker();
-//flusing data from node
-//nControl.flush();
-nControl.flushUpadate = function(){
-	db.sales.find({}).sort({
-		"entrynum": 1
-	, }).exec(function (err, docs) {
-		for (var i = 0, j = docs.length; i < j; i++) {
-			var date = docs[i].date;
-			var currentDate = Date();
-			var d = new Date(date);
-			var u = new Date(currentDate);
-			var dateDays = (d.getMonth() + 1)*30+d.getDate()+d.getFullYear()*365;
-			var currentDateDays = (u.getMonth() + 1)*30+u.getDate()+u.getFullYear()*365;
-			var dayDiff = currentDateDays - dateDays;
-			if(dayDiff>0){
-				db.sales.update({
-		                entrynum: i
-	                          }, {
-		                 $set: {
-			               flushflag: 1
-		                 }
-
-			           })
-			}
-}})};
-
-// inserting id and password to node.db
-$('.modalsetingssave').click(function () {
-	//get the data from the user
-	var id = $('.idd').val();
-	var password = $('.password').val();
-//	save the data received from the user into the db.node
-	db.node.insert([
-		{
-			id: id
-			, password: password
-
-            }
-        ]);
-
-	});
-// removinf sales in machine
-nControl.removeSales = function(){
-	db.sales.remove ({flushflag: 1}, {multi: true}, function (err, numRemoved) {
-		alert(numRemoved);
-
-	});
-
-};
 
 
 
